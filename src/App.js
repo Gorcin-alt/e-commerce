921c081b2f41f154bfd51d281cecc1ced9fac627
Gorcin Knezevic <62382227+Gorcin-alt@users.noreply.github.com>
2021-12-25 18:25:29 +0100

    my-first-push

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Homepage from "./components/Homepage.js"
+
+
+function App() {
+  return (
+    <div >
+      <h1>Hello World</h1>
+      <Homepage />
+
+    </div>
+  );
+}
+
+export default App;
